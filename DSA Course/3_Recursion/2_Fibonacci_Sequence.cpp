03b12563f12f40e0a6325d8eb824e567bd8a199e
vineet <vineet@vrikster.com>
2022-10-13 16:39:13 +0530

    DSA questions added

---
+++ DSA Course/3_Recursion/2_Fibonacci_Sequence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// Recursive implementation to calculate the n-th Fibonacci number
+int fibonacci(int n) {
+    // Base case: n = 0 or 1
+    if (n <= 1) {
+        return n;
+    }
+    // Recursive case: fib(n) = fib(n - 1) + fib(n - 1)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(){
+    int a{9};
+    cout<<fibonacci(a);
+}
