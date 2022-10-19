2a6a97e6fc1d876bc41a0feb350d617a0400c547
vineet <vineet@vrikster.com>
2022-10-19 21:27:50 +0530

    Did a Sorting array question with insertion Sort Method

--- 1_Code_testing.cpp
+++ 1_Code_testing.cpp
@@ -13,4 +13,20 @@
     •
 
 */
-
+#include<vector>
+using std::vector;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for(int i = 1; i<nums.size();i++){
+            int j = i-1;
+            while(j>=0 && nums[j+1] < nums[j]){
+                int temp = nums[j];
+                nums[j]= nums[j+1];
+                nums[j+1] = temp;
+                j--;
+            }
+        }
+        return nums;
+    }
+};
---
+++ Questions (Topic Wise)/Sorting/Sorting_array.cpp
@@ -0,0 +1,32 @@
+// more other solutions
+// https://leetcode.com/problems/sort-an-array/discuss/1401412/C%2B%2B-Clean-Code-Solution-or-Fastest-or-All-(15%2B)-Sorting-Methods-or-Detailed
+
+#include<vector>
+#include<algorithm>
+using std::vector;
+
+// solution works but time limits exceeds on large values
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for(int i = 1; i<nums.size();i++){
+            int j = i-1;
+            while(j>=0 && nums[j+1] < nums[j]){
+                int temp = nums[j];
+                nums[j]= nums[j+1];
+                nums[j+1] = temp;
+                j--;
+            }
+        }
+        return nums;
+    }
+};
+
+//Solution passes and the most simple
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
