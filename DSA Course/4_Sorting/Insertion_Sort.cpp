c7b950b06c7f603910d01369172e894fee4c01c8
vineet <vineet@vrikster.com>
2022-10-19 19:58:46 +0530

    Understood about Insertion Sort

--- DSA Course/4_Sorting/Insertion_Sort.cpp
+++ DSA Course/4_Sorting/Insertion_Sort.cpp
@@ -1,3 +1,14 @@
+/*
+
+    In insertion Sort we are comparing the values and arranging it in accending order or decending
+    order by comparing the 2 values and if the value is found to be smaller than the previous value
+    we keep replacing positions and that smaller is in the right position
+
+    There are 2 types of Insertion Sort
+    1) Stable - where the order dosent change if there its like 2,3,4,4
+    2) Unstable - where the last 4 can come before the first 4 even tho they both are same its
+                  a waste of time and will be counted in a unstable sort
+*/
 #include <vector>
 
 using std::vector;
